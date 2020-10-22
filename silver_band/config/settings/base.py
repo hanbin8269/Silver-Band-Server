71bdc3af4e5648164f0426fd15b1d2b73c6d76e9
hanbin8269 <hanbin8269@gmail.com>
2020-10-22 14:16:55 +0900

    fix base.py and env variable method

--- silver_band/config/settings/base.py
+++ silver_band/config/settings/base.py
@@ -16,25 +16,7 @@ if os.path.isfile(CONFIG_SECRET_COMMON_FILE):
     config_secret_common = json.loads(open(CONFIG_SECRET_COMMON_FILE).read())
 else:
     # 테스팅 환경 (환경변수로 지정해야댐)
-    config_secret_common = {
-        "django": {
-            "secret_key": os.environ["DJANGO_SECRET_KEY"],
-            "database": {
-                "default": {
-                    "ENGINE": "django.db.backends.mysql",
-                    "NAME": "silverbanddb",
-                    "USER": os.environ["DB_USER"],
-                    "PASSWORD": os.environ["DB_PASSWORD"],
-                    "HOST": os.environ["DB_HOST"],
-                    "PORT": "3306",
-                }
-            },
-        },
-        "jwt": {
-            "secret_key": os.environ["JWT_SECRET_KEY"],
-            "algorithm": os.environ["JWT_SECRET_KEY"],
-        },
-    }
+    config_secret_common = json.loads(os.environ["SECRET_SETTING"])
 
 SECRET_KEY = config_secret_common["django"]["secret_key"]
 
