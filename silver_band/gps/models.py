0b35f1a15b1c42a44087cda50978e6a1a3952df2
hanbin8269 <hanbin8269@gmail.com>
2020-10-27 17:28:46 +0900

    feat. models.py Location

--- silver_band/gps/models.py
+++ silver_band/gps/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    wearer = models.ForeignKey("account.Wearer", on_delete=models.CASCADE, null=False)
+    loc_x = models.FloatField()
+    loc_y = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
