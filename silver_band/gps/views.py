fe193199b3337f884324e84bcd42d21b7a14990d
hanbin8269 <hanbin8269@gmail.com>
2020-10-27 19:06:00 +0900

    feat. location viewset and serializer

--- silver_band/gps/views.py
+++ silver_band/gps/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import LocationSerializer
+from .models import Location
 
-# Create your views here.
+
+class LocationViewSet(viewsets.ViewSet):
+    serializer_class = LocationSerializer
+    queryset = Location.objects.all()
